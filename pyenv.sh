2ebd38e4c688e7cf738ea4bfe46e7e9cfe093c48
Duke hsrit02 <daletran@ucdavis.edu>
2021-10-26 16:24:01 +0000

    added sudo validation

--- pyenv.sh
+++ pyenv.sh
@@ -10,8 +10,18 @@
 #I. Build Dependencies
 ## https://realpython.com/intro-to-pyenv/#build-dependencies
 ## https://github.com/pyenv/pyenv/wiki#suggested-build-environment
-sudo apt-get update
-sudo apt-get install -y \
+
+echo "You must use sudo to install. Ctrl+C to cancel."
+# https://stackoverflow.com/questions/3231804/in-bash-how-to-add-are-you-sure-y-n-to-any-command-or-alias
+read -r -p "Are you sudo? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    true
+else
+    exit 1 # exit with error
+fi
+
+sudo apt-get update; sudo apt-get install -y \
 build-essential curl libbz2-dev libffi-dev liblzma-dev libncurses5-dev \
 libncursesw5-dev libreadline-dev libsqlite3-dev libssl-dev libxml2-dev \
 libxmlsec1-dev llvm make python-openssl tk-dev wget xz-utils zlib1g-dev
